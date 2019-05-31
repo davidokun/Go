5161c6e76f68e9f97bc41823a76bb47625818e76
davidokun <davidokun99@gmail.com>
2019-05-31 17:08:19 +0200

    Creating a new Deck

--- examples/slices_and_forloop/deck.go
+++ examples/slices_and_forloop/deck.go
@@ -4,10 +4,26 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck  {
+	cards := deck{}
+
+	cardsSuits := []string{"Hearts", "Spades", "Clubs", "Diamonds"}
+	suitValues := []string{"Ace", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"}
+
+	// The "_" is used to ignore the use of the variable (compiler happy?)
+	for _, suit := range cardsSuits {
+		for _, value := range suitValues {
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 // Receiver for type deck
 // Convention en Go to use only 1 letter for the receiver parameter
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- examples/slices_and_forloop/main.go
+++ examples/slices_and_forloop/main.go
@@ -1,14 +1,8 @@
 package main
 
 func main()  {
-	//Use the declared type in "deck.go"
-	cards := deck{"Ace of Spades", getCard(), "Jack of Souls"}
-	cards = append(cards, "3 of Hearts")
 
-	//Use the defined receiver for type deck
+	cards := newDeck()
+
 	cards.print()
-}
-
-func getCard() string {
-	return "Five of Diamonds"
 }
